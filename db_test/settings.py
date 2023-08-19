1016ecc9adcfff90fbcff4d80d93760897f559f7
gustave-studio <gustave0222@gmail.com>
2023-08-19 22:31:37 +0900

    ✨テストアプリ追加

--- db_test/settings.py
+++ db_test/settings.py
@@ -73,8 +73,13 @@ WSGI_APPLICATION = 'db_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+
     }
 }
 
