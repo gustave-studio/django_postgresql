bd98d8fca9851771ef3a3f03cbee4db77d350de7
gustave-studio <gustave0222@gmail.com>
2023-08-20 00:30:19 +0900

    ✨PostgreSQLからURLを取得して表示する

--- pg_test/views.py
+++ pg_test/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 def moviefunc(request):
-    return render(request, 'movie.html')
+    movie = Movie.objects.get(id=1)
+    return render(request, 'movie.html', {'title': movie.title, 'url': movie.url})
